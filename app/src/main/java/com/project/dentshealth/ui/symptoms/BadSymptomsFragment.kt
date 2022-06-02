22620151fc8f17bb9d746202ae4d7010f328aceb
WidianaAR <widia.war@gmail.com>
2022-06-02 16:35:18 +0800

    change language and get user input text

--- app/src/main/java/com/project/dentshealth/ui/symptoms/BadSymptomsFragment.kt
+++ app/src/main/java/com/project/dentshealth/ui/symptoms/BadSymptomsFragment.kt
@@ -5,56 +5,29 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.project.dentshealth.MainActivity
 import com.project.dentshealth.R
+import com.project.dentshealth.databinding.FragmentBadSymptomsBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+class BadSymptomsFragment : Fragment(), View.OnClickListener {
+    private lateinit var binding: FragmentBadSymptomsBinding
 
-/**
- * A simple [Fragment] subclass.
- * Use the [BadSymptomsFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class BadSymptomsFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentBadSymptomsBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bad_symptoms, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.tlUserSymptoms.setEndIconOnClickListener(this)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment BadSymptomsFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            BadSymptomsFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onClick(v: View?) {
+        val data = binding.tiUserSymptoms.text
+        showText(data.toString())
+    }
+
+    private fun showText(text: String) {
+        Toast.makeText(this.context, text, Toast.LENGTH_SHORT).show()
     }
 }
